b9502da039da2a358c87192c6368364056dd7ff6
mahr <mahaoran0411@gmail.com>
2017-05-20 14:11:53 +0800

    edit/destroy button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    @post.update(post_params)
+    redirect_to account_posts_path, notice:"update success"
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = @group.posts.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Delete success"
+    redirect_to account_posts_path
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
